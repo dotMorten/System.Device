ab125664be06601b59c28623d77a4f7857688148
dotMorten <mn@iter.dk>
2019-10-17 23:12:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### System.Device
+
+.NET Core 3.0 port of System.Device.Location APIs from .NET Reference Source.
+
+Based on commit: https://github.com/microsoft/referencesource/commit/bf498ea2b1a6270a2fe5cb122acf4b1c5b45c21d
+
+Original source code is MIT. See: https://github.com/microsoft/referencesource/blob/master/LICENSE.txt
---
+++ src/TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watcher = new System.Device.Location.GeoCoordinateWatcher(System.Device.Location.GeoPositionAccuracy.High);
+            watcher.StatusChanged += (s, e) => Console.WriteLine($"Status changed: {e.Status}");
+            watcher.PositionChanged += (s, e) => Console.WriteLine($"Position changed: {e.Position.Location.Latitude} , {e.Position.Location.Longitude} , {e.Position.Location.Altitude}  ");
+            watcher.Start();
+            Console.ReadKey();
+            watcher.Stop();
+        }
+    }
+}
